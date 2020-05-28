cce7b571be41563f5f91979d86aee0b706809b62
Ernanda Rully Novrisanti <EN40085X@cimbniaga.co.id>
2020-05-28 12:00:15 +0700

    course (28 May) - encapsulation & overloading

--- OOP/src/App.java
+++ OOP/src/App.java
@@ -1,14 +1,33 @@
-package OOP.src;
+package OOP.src; 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
 
-        int salary = 50_000;
-        int overtime = 5;
-        int payPerHour = 100;
+        // int salary = 50_000;
+        // int overtime = 5;
+        // int payPerHour = 100;
 
-        System.out.println(calculateWage(salary, overtime, payPerHour));
+        // System.out.println(calculateWage(salary, overtime, payPerHour));
+
+        Employee orang = new Employee(50_000, 100);
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Input Salary: ");
+        // orang.setSalary(scanner.nextInt());
+
+        // System.out.print("Input Overtime: ");
+        //orang.setOvertime(scanner.nextInt());
+
+        // System.out.print("Input Pay per Hour: ");
+        // orang.setPayPerHour(scanner.nextInt());
+
+        //System.out.println(calculateWage(orang.getSalary(), orang.getOvertime(), orang.getPayPerHour()));
+
+        System.out.println(orang.calculateWage());
+       // System.out.println(Employee.employeeCount);
+
     }
 
 
